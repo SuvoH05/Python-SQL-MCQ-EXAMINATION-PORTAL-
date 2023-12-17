86454ef74fb5a758f44913c48fdd8c5dce22358b
rcbipasha <rcbipasha66@gmail.com>
2023-12-17 10:26:57 +0530

    Update mcq_portal.py
    1

--- mcq_portal.py
+++ mcq_portal.py
@@ -1,17 +1,34 @@
-import mysql.connector
+import mysql.connector as mq
 
-def registration(captcha,myc):
-    def l_teacher():
-        Username = input("Enter your name: ")
-        Password = input("Enter your password: ")
-        Confirm_password = input("Confirm your password: ")
-
-        if Password != Confirm_password:   
-            print("Passwords do not match.")
-            password = input("Re-enter new password: ")
-            Confirm_password = input("Re-confirm password: ")
-        elif len(password)<8 or len(password)>30:
-            print("Password should be between 8 and 30 characters long.")
-            password = input("Re-enter new password: ")
-            Confirm_password = input("Re-confirm password: ")
-            else:
+def Tdetails():
+    mydb=mq.connect(host="localhost",user="root",password="12345",database="cbse24",auth_plugin='mysql_native_password')
+    myc=mydb.cursor()
+    myc.execute("drop table if exists Tdetails ")
+    myc.execute("create table Tdetails(uid int, TID int, Name varchar(35), dob date, Salary float, doj date, Subject varchar(50), Contact bigint, primary key(TID),foreign key(uid) references Tlogin(uid))")
+    myc.execute("commit")
+    myc.close()
+def TLogin():
+    mydb=mq.connect(host="localhost",user="root",password="12345",database="cbse24",auth_plugin='mysql_native_password')
+    myc=mydb.cursor()
+    myc.execute("drop table if exists Tdetails ")
+    myc.execute("drop table if exists TLogin ")
+    myc.execute("create table TLogin(uid int, pwd varchar(10), primary key(uid))")
+    myc.execute("commit")
+def Sdetails():
+    mydb=mq.connect(host="localhost",user="root",password="12345",database="cbse24",auth_plugin='mysql_native_password')
+    myc=mydb.cursor()
+    myc.execute("drop table if exists Sdetails ")
+    myc.execute("create table Sdetails(uid int, SID int, Name varchar(35), class int, dob date, Contact bigint, primary key(SID),foreign key(uid) references Slogin(uid))")
+    myc.execute("commit")
+    myc.close()
+def SLogin():
+    mydb=mq.connect(host="localhost",user="root",password="12345",database="cbse24",auth_plugin='mysql_native_password')
+    myc=mydb.cursor()
+    myc.execute("drop table if exists Sdetails ")
+    myc.execute("drop table if exists SLogin ")
+    myc.execute("create table SLogin(uid int, pwd varchar(10), primary key(uid))")
+    myc.execute("commit")
+TLogin()
+Tdetails()
+SLogin()
+Sdetails()
